af3966ffb55437f906ad41426e6f7c2513861a2b
gouravdhiman01 <dhimangourav636@gmail.com>
2025-11-13 15:58:10 +0530

    Add files via upload

---
+++ HierarchicalInheritanceDemo.java
@@ -0,0 +1,58 @@
+class Person {
+    String name;
+    int age;
+
+    void inputPerson(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void displayPerson() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+class Student extends Person {
+    int rollNumber;
+
+    void inputStudent(String name, int age, int rollNumber) {
+        inputPerson(name, age);
+        this.rollNumber = rollNumber;
+    }
+
+    void displayStudent() {
+        System.out.println("----- Student Details -----");
+        displayPerson();
+        System.out.println("Roll Number: " + rollNumber);
+    }
+}
+
+class Teacher extends Person {
+    String subjectCode;
+
+    void inputTeacher(String name, int age, String subjectCode) {
+        inputPerson(name, age);
+        this.subjectCode = subjectCode;
+    }
+
+    void displayTeacher() {
+        System.out.println("----- Teacher Details -----");
+        displayPerson();
+        System.out.println("Subject Code: " + subjectCode);
+    }
+}
+
+public class HierarchicalInheritanceDemo {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.inputStudent("Gourav", 20, 101);
+        s.displayStudent();
+
+        System.out.println();
+
+        Teacher t = new Teacher();
+        t.inputTeacher("Mr. Sharma", 40, "CS101");
+        t.displayTeacher();
+    }
+}
